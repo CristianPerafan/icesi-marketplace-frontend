c67c3db4843e3fc4eb4baa9423ed832f13904104
CristianPerafan <1110282139@u.icesi.edu.co>
2024-05-18 13:30:19 -0500

    Feat:update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
 					fontSans.variable
 				)}
 			>
-				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+				<Providers themeProps={{ attribute: "class", defaultTheme: "light" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navigationbar />
 						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
@@ -52,8 +52,7 @@ export default function RootLayout({
 								href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
 								title="nextui.org homepage"
 							>
-								<span className="text-default-600">Powered by</span>
-								<p className="text-primary">NextUI</p>
+
 							</Link>
 						</footer>
 					</div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,31 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+
 
 export default function Home() {
 	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-				<h1 className={title()}>Make&nbsp;</h1>
-				<h1 className={title({ color: "violet" })}>beautiful&nbsp;</h1>
-				<br />
-				<h1 className={title()}>
-					websites regardless of your design experience.
+		<section className="flex flex-col items-center justify-center gap-2 py-4 md:py-4 md:flex-row ">
+			<div className="inline-block max-w-lg text-left justify-left">
+				<div className="flex justify-left space-x-2 ">
+					<h1 className="text-4xl lg:text-6xl">Tu</h1>
+					<h1 className="text-4xl lg:text-6xl text-blue-700"> Comunidad,</h1>
+				</div>
+				<h1 className="text-4xl lg:text-6xl">
+					tus productos.
 				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Beautiful, fast and modern React UI library.
-				</h2>
+				<br />
+
+				<p className="text-lg lg:text-xl text-default-600 block max-w-full">
+					Descubre la tienda virtual de nuestra universidad: donde la comunidad vende y compra.
+				</p>
+
 			</div>
 
+
+
 			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
+				<img className="" src="https://demo.themesberg.com/landwind/images/hero.png"></img>
 			</div>
 
 			<div className="mt-8">
-				<Snippet hideSymbol hideCopyButton variant="flat">
-					<span>
-						Get started by editing <Code color="primary">app/page.tsx</Code>
-					</span>
-				</Snippet>
 			</div>
 		</section>
 	);

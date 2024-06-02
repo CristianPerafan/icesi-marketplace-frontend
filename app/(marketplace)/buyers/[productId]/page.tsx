29328d9d43fdb7d9b2af6523c43ac4120c0e8ea1
juandavidbaro <juandavidbaro@gmail.com>
2024-06-02 14:20:36 -0500

    Add cart with some problems

--- app/(marketplace)/buyers/[productId]/page.tsx
+++ app/(marketplace)/buyers/[productId]/page.tsx
@@ -1,13 +1,19 @@
 "use client";
 import { useParams } from 'next/navigation';
 import { getProductById } from '@/services/buyers';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { ProductEntity } from '@/model/product.entity';
 import { Card, CardBody, CardFooter, Image } from '@nextui-org/react';
+import CartContext from '@/context/cartContext';
+
+
 
 function ProductDetail() {
   const params = useParams();
   const [product, setProduct] = useState<ProductEntity | null>(null);
+  
+  // Obtener el contexto del carrito
+  const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
     if (params.productId) {
@@ -21,17 +27,13 @@ function ProductDetail() {
     }
   }, [params.productId]);
 
-  const handleButtonClick = () => {
-    // Handle button click action here
-  };
-
   if (!product) {
     return <div>Loading...</div>;
   }
 
   return (
     <div className="flex justify-center items-center" style={{ height: '100vh' }}>
-      <div onClick={handleButtonClick}>
+      <div>
         <Card shadow="sm" className="custom-card">
           <CardBody className="overflow-visible p-0">
             <Image
@@ -48,10 +50,8 @@ function ProductDetail() {
             <b className="mb-2">{product.name}</b> 
             <p className="text-default-500">{`Precio: $${product.price.toLocaleString()} COP`}</p>
             <p>{product.description}</p>
-            <div className="flex justify-between">
-              <button className='mt-4 bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded-lg transition-colors duration-300'style={{ marginRight: '8px' }}>Comprar</button>
-              <button className='mt-4 bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded-lg transition-colors duration-300 '>Añadir al carrito</button>
-            </div>
+            {/* Agregar el producto al carrito */}
+            <button className='mt-4 bg-blue-400 hover:bg-blue-500 text-white py-2 px-4 rounded-lg transition-colors duration-300' onClick={() => addToCart(product)}>Añadir al carrito</button>
           </CardFooter>
         </Card>
       </div>
--- app/(marketplace)/buyers/products/page.tsx
+++ app/(marketplace)/buyers/products/page.tsx
@@ -1,9 +1,14 @@
 "use client";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { getBuyersProducts } from '@/services/buyers';
 import { ProductEntity } from "@/model/product.entity";
-import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
-import Link from 'next/link'; 
+import { Button, Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import CartContext from '@/context/cartContext';
+import { BuyerRoutes } from '@/config/routes';
+
+
 
 // Mapeo de categorías a imágenes
 const categoryImages: Record<string, string> = {
@@ -18,6 +23,10 @@ const categoryImages: Record<string, string> = {
 
 function Buyers() {
   const [products, setProducts] = useState<ProductEntity[]>([]);
+  const router = useRouter();
+
+  // Obtener el contexto del carrito
+  const { addToCart } = useContext(CartContext);
 
   useEffect(() => {
     getBuyersProducts().then((data) => {
@@ -32,31 +41,36 @@ function Buyers() {
   }, []);
 
   return (
-    <div className="flex justify-center"> 
-      <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4">
-        {products.map((product) => (
-          <div key={product.id}>
-            <Link href={`/buyers/${product.id}`}>
-              <Card shadow="sm" isPressable className="custom-card">
-                <CardBody className="overflow-visible p-0">
-                  <Image
-                    shadow="sm"
-                    radius="lg"
-                    width="100%"
-                    alt={product.name}
-                    className="w-full object-cover"
-                    style={{ height: '200px', maxWidth: '300px' }}
-                    src={product.img}
-                  />
-                </CardBody>
-                <CardFooter className="p-4 flex flex-col items-start"> 
-                  <b className="mb-2">{product.name}</b> 
-                  <p className="text-default-500">{`$${product.price.toLocaleString()} COP`}</p>
-                </CardFooter>
-              </Card>
-            </Link>
-          </div>
-        ))}
+    <div className="relative">
+      <div className="absolute top-4 right-4">
+        <Button className="bg-orange-500 py-4 px-10" auto flat onPress={() => router.push(BuyerRoutes.STORE)}>Ver Carrito</Button>
+      </div>
+      <div className="flex justify-center">
+        <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 p-4">
+          {products.map((product) => (
+            <div key={product.id}>
+              <Link href={`/buyers/${product.id}`}>
+                <Card shadow="sm" isPressable className="custom-card">
+                  <CardBody className="overflow-visible p-0">
+                    <Image
+                      shadow="sm"
+                      radius="lg"
+                      width="100%"
+                      alt={product.name}
+                      className="w-full object-cover"
+                      style={{ height: '200px', maxWidth: '300px' }}
+                      src={product.img}
+                    />
+                  </CardBody>
+                  <CardFooter className="p-4 flex flex-col items-start">
+                    <b className="mb-2">{product.name}</b>
+                    <p className="text-default-500">{`$${product.price.toLocaleString()} COP`}</p>
+                  </CardFooter>
+                </Card>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
